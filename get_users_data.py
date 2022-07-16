a4e5367facd3707fbbb0b0434c45c91c196395ed
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-16 22:58:01 +0500

    get_users_data

--- get_users_data.py
+++ get_users_data.py
@@ -12,3 +12,11 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
+    results = data["results"]
+    list1 = []
+    for i in results:
+         list1.append({"first_name":i['name']['first'], "Last_name":i['name']['last'], "Phone":i['phone']})
+    return list1
+
+data = get_data('randomuser_data.json')
+print(get_users_data(data))
